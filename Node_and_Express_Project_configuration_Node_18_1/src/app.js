21331d079f6957697643f0f6399eac7065950e90
Ethan <76500833@protonmail.com>
2025-02-06 15:46:01 -0500

    learning about params

--- Node_and_Express_Project_configuration_Node_18_1/src/app.js
+++ Node_and_Express_Project_configuration_Node_18_1/src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 
 module.exports = app;
--- node-express-getting-started/src/server.js
+++ node-express-getting-started/src/server.js
@@ -1,5 +1,17 @@
 const { PORT = 5000 } = process.env
-
+const morgan = require('morgan')
 const app = require("./app")
+app.use(morgan('dev'));
 
+    const sayHello = (req, res) => {
+        console.log(req.query);
+        const name = req.query.name;
+        const content = name ? `Hello, ${name}!` : "Hello!";
+        res.send(content);
+      };
+
+app.get("/songs", (req, res) => {
+  const title = req.query.title;
+  res.send(title);
+});
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
