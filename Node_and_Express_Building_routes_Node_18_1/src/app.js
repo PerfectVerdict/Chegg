63681ec037ef6e89d2998e2ee57c3c19cdebe904
PerfectVerdict <perfectverdict@protonmail.com>
2025-02-06 16:23:16 -0500

    had to look at solution

--- Node_and_Express_Building_routes_Node_18_1/src/app.js
+++ Node_and_Express_Building_routes_Node_18_1/src/app.js
@@ -1,24 +1,62 @@
-const { NODE_ENV = "development" } = process.env;
+// const { NODE_ENV = "development" } = process.env;
+// const express = require("express");
+// const app = express();
+// 
+// app.get("/say/:dynamic", (req, res) => {
+// 	const dynamic = req.params.dynamic
+// 	const result = `${dynamic}!!!`
+// 	res.send(result)
+// });
+// 
+// app.get("/say/goodbye", (req, res) => {
+// 	res.send("sorry to see you go")
+// })
+// 
+// 
+// 
+// app.get("/hello", (req, res) => {
+// 	console.log(req.query)
+// 	const name = req.query.name;
+// 	const content = name ? `Hello, ${name}!` : "Hello, no name!";
+// });
+// 
+// app.get('/songs', (req, res) => {
+// 	const title = req.query.title;
+// 	res.send(title);
+// })
+// module.exports = app;
+
+
+
 const express = require("express");
+// const morgan = require("morgan");
 const app = express();
-// Build your routes here
-const sayHello = (req, res) => {
-    console.log(req.query);
-    const name = req.query.name;
-    const content = name ? `Hello, ${name}!` : "Hello!";
-    res.send(content);
-  };
 
-const saySomething = (req, res) => {
-	const dynamic = req.params.dynamic
-	const result = `${dynamic}!`
-	res.send(result)
-}
+// Application-level middleware
+// app.use(morgan("dev"));
 
-app.get('/say/:dynamic', saySomething) 
+// Route functions
+const sayHello = (req, res, next) => {
+  const name = req.query.name;
+  const content = name ? `Hello, ${name}!` : "Hello!";
+  res.send(content);
+};
 
-app.get('/songs', (req, res) => {
-	const title = req.query.title;
-	res.send(title);
-})
+const sayGoodbye = (req, res, next) => {
+  res.send("Sorry to see you go!");
+};
+
+const saySomething = (req, res, next) => {
+  const greeting = req.params.greeting;
+  const name = req.query.name;
+
+  const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+  res.send(content);
+};
+
+// Routes
+app.get("/hello", sayHello);
+app.get("/say/goodbye", sayGoodbye);
+app.get("/say/:greeting", saySomething);
+
 module.exports = app;
